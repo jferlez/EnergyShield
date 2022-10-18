38fa4fa732da1a3088dce0e4fb5c69956144456e
James Ferlez <jferlez@gmail.com>
2022-10-18 11:59:26 -0700

    Encapsulate delta T calculations in a class
    This should allow the loading of different parameters for different shields much more easily

--- deltaTCalculations/energyShieldDeltaT.py
+++ deltaTCalculations/energyShieldDeltaT.py
@@ -4,51 +4,53 @@ import numpy as np
 import pickle
 import math
 
-thismodule = sys.modules[__name__]
+class energyShieldDeltaT:
 
-with open('energyShieldPyLUT.p','rb') as fp:
-    lut = pickle.load(fp)
-assert len(lut) > 0, 'ERROR: LUT is empty'
-lut.sort(key=(lambda x: x['offs']))
+    def __init__(self,fileName):
 
-offsets = np.array([ lutObj['offs'] for lutObj in lut], dtype=np.float64)
+        with open(fileName,'rb') as fp:
+            self.lut = pickle.load(fp)
+        assert len(self.lut) > 0, 'ERROR: LUT is empty'
+        self.lut.sort(key=(lambda x: x['offs']))
 
-paramList = ['rbar', 'sigma', 'vmax', 'lr', 'deltaFMax', 'betaMax']
-for p in paramList:
-    setattr(thismodule, p, lut[0][p])
-    assert all([lutObj[p] == getattr(thismodule, p) for lutObj in lut]), f'ERROR: LUT contains different values for problem parameter \'{p}\'...'
+        self.offsets = np.array([ self.lutObj['offs'] for self.lutObj in self.lut], dtype=np.float64)
 
-def rmin(xi):
-    return rbar/(sigma*np.cos(xi/2) + 1 - sigma)
+        paramList = ['rbar', 'sigma', 'vmax', 'lr', 'deltaFMax', 'betaMax']
+        for p in paramList:
+            setattr(self, p, self.lut[0][p])
+            assert all([self.lutObj[p] == getattr(self, p) for self.lutObj in self.lut]), f'ERROR: lut contains different values for problem parameter \'{p}\'...'
 
-def deltaT(r=0, xi=0, beta=0, debug=False):
-    assert xi >= -np.pi and xi <= np.pi, f'ERROR: xi = {xi} is outside of the range [-pi, pi]'
-    assert beta >= -betaMax and beta <= betaMax, f'ERROR: beta = {beta} is outside of the range [-{betaMax}, {betaMax}]'
+    def rmin(self, xi):
+        return self.rbar/(self.sigma*np.cos(xi/2) + 1 - self.sigma)
 
-    validOffsets = r - (rmin(xi) + offsets)
-    validOffsetIdxs = np.nonzero( (r - (rmin(xi) + offsets)) > 0 )[0]
-    if debug:
-        print(f'DEBUG: offset comparisons {validOffsets}')
-        print(f'DEBUG: validOffsets = {validOffsetIdxs}')
-    if len(validOffsetIdxs) > 0:
-        offsetIdx = validOffsetIdxs[ np.argmin(validOffsets[validOffsetIdxs]) ]
-        offset = lut[offsetIdx]['offs']
-    else:
-        return 0
-    if debug:
-        print(f'DEBUG: using offsetIdx = {offsetIdx} with offset = {offset}; rmin({xi}) = {rmin(xi)}')
-    xiIncrement = lut[offsetIdx]['xiIncrement']
-    betaIncrement = lut[offsetIdx]['betaIncrement']
+    def deltaT(self, r=0, xi=0, beta=0, debug=False):
+        assert xi >= -np.pi and xi <= np.pi, f'ERROR: xi = {xi} is outside of the range [-pi, pi]'
+        assert beta >= -self.betaMax and beta <= self.betaMax, f'ERROR: beta = {beta} is outside of the range [-{self.betaMax}, {self.betaMax}]'
 
-    xiIndex = int(np.floor((xi+np.pi)/xiIncrement))
-    assert xiIndex >=0 and xiIndex < len(lut[offsetIdx]['xiPoints']), 'INTERNAL ERROR: xiIndex out of range'
-    betaIndex = int(np.floor((beta+betaMax)/betaIncrement))
-    assert betaIndex >=0 and betaIndex < len(lut[offsetIdx]['betaPoints']), 'INTERNAL ERROR: betaIndex out of range'
+        validOffsets = r - (self.rmin(xi) + self.offsets)
+        validOffsetIdxs = np.nonzero( (r - (self.rmin(xi) + self.offsets)) > 0 )[0]
+        if debug:
+            print(f'DEBUG: offset comparisons {validOffsets}')
+            print(f'DEBUG: validOffsets = {validOffsetIdxs}')
+        if len(validOffsetIdxs) > 0:
+            offsetIdx = validOffsetIdxs[ np.argmin(validOffsets[validOffsetIdxs]) ]
+            offset = self.lut[offsetIdx]['offs']
+        else:
+            return 0
+        if debug:
+            print(f'DEBUG: using offsetIdx = {offsetIdx} with offset = {offset}; rmin({xi}) = {self.rmin(xi)}')
+        xiIncrement = self.lut[offsetIdx]['xiIncrement']
+        betaIncrement = self.lut[offsetIdx]['betaIncrement']
 
-    if debug:
-        xiLeftEndPt = lut[offsetIdx]['xiPoints'][xiIndex]
-        betaLeftEndPt = lut[offsetIdx]['betaPoints'][betaIndex]
-        print(f'DEBUG: xi={xi}; xi interval = [{xiLeftEndPt}, {min(xiLeftEndPt+xiIncrement, np.pi)}]')
-        print(f'DEBUG: beta={beta}; beta interval = [{betaLeftEndPt}, {min(betaLeftEndPt+betaIncrement, betaMax)}]')
+        xiIndex = int(np.floor((xi+np.pi)/xiIncrement))
+        assert xiIndex >=0 and xiIndex < len(self.lut[offsetIdx]['xiPoints']), 'INTERNAL ERROR: xiIndex out of range'
+        betaIndex = int(np.floor((beta+self.betaMax)/betaIncrement))
+        assert betaIndex >=0 and betaIndex < len(self.lut[offsetIdx]['betaPoints']), 'INTERNAL ERROR: betaIndex out of range'
 
-    return lut[offsetIdx]['lut'][xiIndex, betaIndex]
+        if debug:
+            xiLeftEndPt = self.lut[offsetIdx]['xiPoints'][xiIndex]
+            betaLeftEndPt = self.lut[offsetIdx]['betaPoints'][betaIndex]
+            print(f'DEBUG: xi={xi}; xi interval = [{xiLeftEndPt}, {min(xiLeftEndPt+xiIncrement, np.pi)}]')
+            print(f'DEBUG: beta={beta}; beta interval = [{betaLeftEndPt}, {min(betaLeftEndPt+betaIncrement, self.betaMax)}]')
+
+        return self.lut[offsetIdx]['lut'][xiIndex, betaIndex]
